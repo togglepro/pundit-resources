6ab6a807318f6f0b1bcc531b477b341fa0cabae9
Ross Penman <ross@pen.mn>
2016-05-24 13:39:07 +0100

    Remove sample spec

--- spec/pundit/resources_spec.rb
+++ spec/pundit/resources_spec.rb
@@ -4,8 +4,4 @@ describe Pundit::Resources do
   it 'has a version number' do
     expect(Pundit::Resources::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
