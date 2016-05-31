93955b839bf1713a96fa533a63ca1d9713555c33
Sean Devine <barelyknown@icloud.com>
2016-05-31 15:53:27 -0400

    refactor controller test to pass params in expected way per rails version

--- spec/controllers/enforced_policy_use_spec.rb
+++ spec/controllers/enforced_policy_use_spec.rb
@@ -3,16 +3,24 @@ require "rails_helper"
 RSpec.describe UsersController, type: :controller do
   render_views
 
+  def params_hash(inner_hash)
+    if Rails.version < '5.0.0'
+      inner_hash
+    else
+      { params: inner_hash }
+    end
+  end
+
+  def do_request
+    get :show, params_hash(id: User.create!.id)
+  end
+
   context "when the policy callback is not called" do
     before do
       original = UsersController.instance_method(:context)
       expect_any_instance_of(UsersController).to receive(:context) { |instance|
         original.bind(instance).call.merge(policy_used: -> {})
       }
-    end
-
-    def do_request
-      get :show, params: { id: User.create!.id }
     end
 
     it "raises an exception" do
@@ -22,7 +30,7 @@ RSpec.describe UsersController, type: :controller do
 
   context "when the policy callback is called" do
     it "responds with 200 OK" do
-      get :show, params: { id: User.create!.id }
+      do_request
       expect(response).to have_http_status 200
     end
   end
